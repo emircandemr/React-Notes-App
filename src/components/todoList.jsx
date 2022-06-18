5621220a5eac48d25db08e8b807c39075962a0d3
emircandemr <72731296+emircandemr@users.noreply.github.com>
2022-06-18 15:42:23 +0300

    update

--- src/components/todoList.jsx
+++ src/components/todoList.jsx
@@ -10,7 +10,6 @@ function TodoList({todo,setTodo,filteredTodos}) {
         <div className='todoList  w-5/6 h-auto m-5 p-2 flex justify-center '>
 
              {todo.length<1 ? <div className='mt-20 text-lg text-gray-400 italic tracking-wide'>Please Add Notes to the List</div>: ""} 
-
             {filteredTodos.map( (element) => {
               localStorage.setItem("notes", JSON.stringify(todo));
               return(
